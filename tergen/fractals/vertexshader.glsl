0e17e4f119bd75657e45072cdc4178b7584d0cd2
Patryk Bęza <patryk.beza@gmail.com>
2016-02-03 17:35:34 +0100

    Rysowanie trójkąta

--- tergen/fractals/vertexshader.glsl
+++ tergen/fractals/vertexshader.glsl
@@ -1,7 +1,14 @@
-attribute vec4 vertex;
-attribute vec3 normal;
-varying vec3 vert;
-varying vec3 vertNormal;
+#version 300 es
+
+precision highp vec4;
+precision highp vec3;
+precision highp mat4;
+precision highp mat3;
+
+in vec4 vertex;
+in vec3 normal;
+out vec3 vert;
+out vec3 vertNormal;
 uniform mat4 projMatrix;
 uniform mat4 mvMatrix;
 uniform mat3 normalMatrix;

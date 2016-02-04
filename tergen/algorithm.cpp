cb9f611764c655705fd3a73f6dd34cfc0b7a32bb
Coolthulhu <Coolthulhu@gmail.com>
2016-02-04 02:09:37 +0100

    Liczenie minkowskiego

--- tergen/algorithm.cpp
+++ tergen/algorithm.cpp
@@ -1,5 +1,6 @@
 #include "fract.h"
 #include <cstdio>
+#include <cmath>
 
 point_container::point_container( size_t w, size_t h )
 {
@@ -45,9 +46,25 @@ int main( int argc, char **argv )
     test.set( size/2, size/2, 1.0f );
     print_container( test );
     printf( "\n\n" );
-    point_container test2 = single_iter( test, 0.0 );
-    print_container( test2 );
+    point_container test2 = multi_iter( test, 2, 0.0 );
+    //print_container( test2 );
+    printf( "\n\n" );
+    box_dimension( test2, 9 );
     return 0;
+}
+
+point_container multi_iter( const point_container &points, size_t iters, float s )
+{
+    point_container pts = points;
+
+    while( true ) {
+        if( iters == 0 ) {
+            return pts;
+        }
+
+        pts = std::move( single_iter( pts, s ) );
+        iters--;
+    }
 }
 
 point_container single_iter( const point_container &points, float s )
@@ -135,4 +152,195 @@ point_container single_iter( const point_container &points, float s )
     }
 
     return new_data;
+}
+
+inline size_t index_from_3d( int x, int y, int z, int side_len )
+{
+    if( x < 0 || y < 0 || z < 0 || side_len < 0 ) {
+        printf("err");
+        return 0;
+    }
+    size_t ret = ((x * side_len) + y) * side_len + z;
+    if( ret >= side_len * side_len * side_len ) {
+        return 0;
+    }
+
+    return (size_t)ret;
+}
+
+inline void draw( int x, int y, int z, int side_len,
+                  std::vector<bool> &boxed, size_t &count )
+{
+    size_t index = index_from_3d( x, y, z, side_len );
+    if( !boxed[index] ) {
+        count++;
+        boxed[index] = true;
+    }
+}
+
+void line( int x1, int y1, int z1,
+           const int x2, const int y2, const int z2,
+           int side_len, std::vector<bool> &boxed, size_t &count )
+{
+    
+    int i, dx, dy, dz, l, m, n, x_inc, y_inc, z_inc, err_1, err_2, dx2, dy2, dz2;
+
+    int x = x1;
+    int y = y1;
+    int z = z1;
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dz = z2 - z1;
+    x_inc = (dx < 0) ? -1 : 1;
+    l = abs(dx);
+    y_inc = (dy < 0) ? -1 : 1;
+    m = abs(dy);
+    z_inc = (dz < 0) ? -1 : 1;
+    n = abs(dz);
+    dx2 = l << 1;
+    dy2 = m << 1;
+    dz2 = n << 1;
+    
+    if ((l >= m) && (l >= n)) {
+        err_1 = dy2 - l;
+        err_2 = dz2 - l;
+        for (i = 0; i < l; i++) {
+            draw( x, y, z, side_len, boxed, count );
+            if (err_1 > 0) {
+                y += y_inc;
+                err_1 -= dx2;
+            }
+            if (err_2 > 0) {
+                z += z_inc;
+                err_2 -= dx2;
+            }
+            err_1 += dy2;
+            err_2 += dz2;
+            x += x_inc;
+        }
+    } else if ((m >= l) && (m >= n)) {
+        err_1 = dx2 - m;
+        err_2 = dz2 - m;
+        for (i = 0; i < m; i++) {
+            draw( x, y, z, side_len, boxed, count );
+            if (err_1 > 0) {
+                x += x_inc;
+                err_1 -= dy2;
+            }
+            if (err_2 > 0) {
+                z += z_inc;
+                err_2 -= dy2;
+            }
+            err_1 += dx2;
+            err_2 += dz2;
+            y += y_inc;
+        }
+    } else {
+        err_1 = dy2 - n;
+        err_2 = dx2 - n;
+        for (i = 0; i < n; i++) {
+            draw( x, y, z, side_len, boxed, count );
+            if (err_1 > 0) {
+                y += y_inc;
+                err_1 -= dz2;
+            }
+            if (err_2 > 0) {
+                x += x_inc;
+                err_2 -= dz2;
+            }
+            err_1 += dy2;
+            err_2 += dx2;
+            z += z_inc;
+        }
+    }
+    draw( x, y, z, side_len, boxed, count );
+}
+
+inline void set_indices( size_t i, size_t j,
+                         size_t &xindex, size_t &yindex, size_t &zindex,
+                         size_t side_len,
+                         const point_container &points )
+{
+    const float x = (float)i / points.w();
+    const float y = (float)j / points.h();
+    const float z = points.get( i, j );
+    xindex = (size_t)( x * side_len );
+    yindex = (size_t)( y * side_len );
+    zindex = (size_t)( z * side_len );
+}
+
+float box_dimension( const point_container &points, const size_t subdiv )
+{
+    int last_side_len = 2 << subdiv;
+    int cur_side_len = last_side_len / 2;
+    std::vector<bool> last;
+    std::vector<bool> cur;
+    std::vector<size_t> counts;
+    std::vector<size_t> lens;
+    size_t cur_count = 0;
+    // 3 sides of the cube
+    last.resize( last_side_len * last_side_len * last_side_len );
+    cur.resize( cur_side_len * cur_side_len * cur_side_len );
+
+    for( size_t i = 1; i < points.w() - 1; i++ ) {
+        for( size_t j = 1; j < points.h() - 1; j++ ) {
+            size_t xindex = 0;
+            size_t yindex = 0;
+            size_t zindex = 0;
+            size_t xindex_n = 0;
+            size_t yindex_n = 0;
+            size_t zindex_n = 0;
+            set_indices( i, j, xindex, yindex, zindex, last_side_len, points );
+
+            set_indices( i - 1, j, xindex_n, yindex_n, zindex_n, last_side_len, points );
+            line( xindex, yindex, zindex, xindex_n, yindex_n, zindex_n, last_side_len, last, cur_count );
+            set_indices( i + 1, j, xindex_n, yindex_n, zindex_n, last_side_len, points );
+            line( xindex, yindex, zindex, xindex_n, yindex_n, zindex_n, last_side_len, last, cur_count );
+            set_indices( i, j - 1, xindex_n, yindex_n, zindex_n, last_side_len, points );
+            line( xindex, yindex, zindex, xindex_n, yindex_n, zindex_n, last_side_len, last, cur_count );
+            set_indices( i, j + 1, xindex_n, yindex_n, zindex_n, last_side_len, points );
+            line( xindex, yindex, zindex, xindex_n, yindex_n, zindex_n, last_side_len, last, cur_count );
+        }
+    }
+
+    counts.push_back( cur_count );
+    lens.push_back( last_side_len );
+    for( size_t cur_div = subdiv - 1; cur_div > 2; cur_div-- ) {
+        cur.clear();
+        cur.resize( cur_side_len * cur_side_len * cur_side_len );
+
+        cur_count = 0;
+
+        for( size_t i = 0; i < last_side_len; i++ ) {
+            for( size_t j = 0; j < last_side_len; j++ ) {
+                for( size_t k = 0; k < last_side_len; k++ ) {
+                    const size_t old_index = index_from_3d( i, j, k, last_side_len );
+                    if( !last[old_index] ) {
+                        continue;
+                    }
+
+                    const size_t new_index = index_from_3d( i / 2, j / 2, k / 2, cur_side_len );
+                    if( !cur[new_index] ) {
+                        cur_count++;
+                    }
+
+                    cur[new_index] = true;
+                }
+            }
+        }
+
+        counts.push_back( cur_count );
+        lens.push_back( cur_side_len );
+        last = std::move( cur );
+        last_side_len /= 2;
+        cur_side_len /= 2;
+    }
+
+    for( size_t i = 0; i < counts.size(); i++ ) {
+        const float boxlen = 1.0f / lens[i];
+        const float mink = counts[i] > 0 ? log( counts[i] ) / log( 1.0 / boxlen ) : 0.0f;
+        printf( "%lu na %lu, minkowski %.2f\n", counts[i], lens[i] * lens[i] * lens[i], mink );
+    }
+
+    return 0;
 }
--- tergen/fract.h
+++ tergen/fract.h
@@ -17,6 +17,10 @@ public:
     point_container( size_t w, size_t h );
     point_container( const std::vector<float> &dat, size_t w, size_t h );
     point_container( point_container &&other ) = default;
+    point_container( const point_container &other ) = default;
+
+    point_container &operator=( point_container &&other ) = default;
+    point_container &operator=( const point_container &other ) = default;
 
     float get( size_t x, size_t y ) const;
     void set( size_t x, size_t y, float val );
@@ -26,5 +30,9 @@ public:
 
 // s must be >-1.0 and <1.0
 point_container single_iter( const point_container &points, float s = 0.0f );
+point_container multi_iter( const point_container &points, size_t iters, float s = 0.0f );
+// Calculates box dimension naively
+// Creates 8^subdiv boxes, so keep subdiv low!
+float box_dimension( const point_container &points, size_t subdiv = 8 );
 
 #endif
